27ce56b5032e16aded7e18ca8fc8d1fbc6102003
Alfo <alf.luna@duocuc.cl>
2025-04-12 13:35:43 -0400

    inicio de controller

--- src/main/java/com/example/biblioteca/controller/libroController.java
+++ src/main/java/com/example/biblioteca/controller/libroController.java
@@ -4,11 +4,23 @@ import com.example.biblioteca.model.libro;
 import com.example.biblioteca.service.LIbroService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
+@RequestMapping("/api/v1/libros")
 public class libroController {
+    @Autowired
+    private LIbroService libroService;
 
+    @GetMapping()
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
 }
